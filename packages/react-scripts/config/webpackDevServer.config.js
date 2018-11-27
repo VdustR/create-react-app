e5bc2943a48aaecf52a685d5926aa731f73fe765
VdustR <VdustR@gmail.com>
2018-11-27 14:58:51 +0800

    fix require path

--- packages/react-scripts/config/webpackDevServer.config.js
+++ packages/react-scripts/config/webpackDevServer.config.js
@@ -12,6 +12,7 @@ const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware')
 const evalSourceMapMiddleware = require('react-dev-utils/evalSourceMapMiddleware');
 const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
 const ignoredFiles = require('react-dev-utils/ignoredFiles');
+const path = require('path');
 const config = require(process.env.WEBPACK_CONFIG
   ? path.join(paths.appPath, process.env.WEBPACK_CONFIG)
   : './webpack.config');
